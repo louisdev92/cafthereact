130d80c0b0ace5d487b9dc3588d16d63b9de32a9
louis.hameau <louis.hameau@campus-centre.fr>
2025-02-10 10:56:39 +0100

    Commit liste produit

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,9 +1,28 @@
-import React from 'react';
-
+import React, {useEffect} from 'react';
+import axios from "axios";
+/* npm install axios */
 function ProductList(props) {
+        const [produits, setProduits] = React.useState([]);
+
+        useEffect(() => {
+            const fetchProduits = async () => {
+                try {
+                    const response = await axios.get("http://localhost:3000/api/produits");
+
+                    setProduits(response.data);
+                } catch (error) {
+                    console.error("Erreur de chargement des produits", error);
+                }
+            };
+
+            void fetchProduits();
+        }, []);
     return (
         <div>
             <h3> Liste des produits</h3>
+            <div className="product-list">
+                {produits.map((produit) => produit.produit_id)}
+            </div>
         </div>
     );
 }
