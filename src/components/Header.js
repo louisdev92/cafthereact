36947648a219964426b8ae8f5d8292a7d37e476e
louis.hameau <louis.hameau@campus-centre.fr>
2025-02-13 16:07:24 +0100

    Commit general

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,52 +1,46 @@
-import React, {useContext, useState} from 'react';
-import {Link, useNavigate} from 'react-router-dom'; // Importation de useNavigate
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import {AuthContext} from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext";
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import '../styles/App.css';
 
 function Header() {
-    const navigate = useNavigate(); // Déclaration du hook navigate
-    const handlelogout = () => {
-        logout();
-    };
-    const{user, isAuthenticated, logout} = useContext(AuthContext);
+    const navigate = useNavigate();
+    const { user, isAuthenticated, logout } = useContext(AuthContext);
 
-    // Fonction pour gérer le clic sur l'icône utilisateur
     const handleUserClick = () => {
-        navigate('/login'); // Redirection vers la route "/login"
+        navigate('/login');
     };
 
     return (
         <header className="header">
             <div className="header-content">
-                <h1 className="brand-title">CAFTHE</h1>
             </div>
             <div className="header-icons">
-                {/* Icône de profil (connexion) */}
-                <FontAwesomeIcon
-                    icon={faUser}
-                    className="icon"
-                    onClick={handleUserClick} // Ajout de l'événement de clic
-                />
-                {/* Icône de panier */}
-                <FontAwesomeIcon icon={faShoppingCart} className="icon"/>
+                <FontAwesomeIcon icon={faUser} className="icon" onClick={handleUserClick} />
+                <FontAwesomeIcon icon={faShoppingCart} className="icon" />
                 <div>
                     {isAuthenticated ? (
                         <>
-                            <span>{user.prenom.toUpperCase()} {user.nom.toUpperCase()}   </span>
-                            <button onClick={handlelogout}>
-                                <FaSignOutAlt />
+                            <span>
+                                {user.prenom.charAt(0).toUpperCase() + user.prenom.slice(1).toLowerCase()}
+                                {" "}
+                                {user.nom.charAt(0).toUpperCase() + user.nom.slice(1).toLowerCase()}
+                                {" "}
+                            </span>
+                            <button className="logout-button" onClick={logout}>
+                                <FaSignOutAlt/>
                             </button>
                         </>
                     ) : (
                         <Link to={'/login'}>
-                            <FaSignInAlt /> Se connecter
+                        <FaSignInAlt/> Se connecter
                         </Link>
                     )}
                 </div>
             </div>
-
         </header>
     );
 }
