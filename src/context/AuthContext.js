24f81e05d9c32704396c410422e5392e478e2265
louis.hameau <louis.hameau@campus-centre.fr>
2025-02-14 11:26:54 +0100

    Commit general

---
+++ src/context/AuthContext.js
@@ -0,0 +1,55 @@
+import React, {createContext, useState, useEffect} from 'react';
+
+/* Exportation du contexte pour y avoir accès*/
+export const AuthContext = createContext(null);
+
+/* Création du provider pour la connexion et la deco */
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null); // Client
+    const [token, setToken] = useState(null); // token JWT
+
+    // Stockage dans le LocalStorage pour la persistance des données
+    useEffect(() => {
+        const storedToken = localStorage.getItem("token");
+        const storedUser = localStorage.getItem("user");
+
+        if(storedUser && storedToken) {
+            setToken(storedToken)
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
+    // Si le token ou l'user changent, on met a jour le LocalStorage
+    useEffect(() => {
+        if (token && user) {
+            localStorage.setItem("token", token);
+            localStorage.setItem("user", JSON.stringify(user));
+        } else {
+            localStorage.removeItem("token");
+            localStorage.removeItem("user");
+        }
+    }, [token, user]);
+
+    // Connexion (On recoit les données envoyées par l'API : token + infos client)
+
+    const login = (jwt, userData) => {
+        setToken(jwt);
+        setUser(userData);
+    };
+    // Deconnexion
+
+    const logout = () => {
+        setToken(null);
+        setUser(null);
+    };
+
+    const value = {
+        token,
+        user,
+        login,
+        logout,
+        isAuthenticated: !!token,
+    }
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
