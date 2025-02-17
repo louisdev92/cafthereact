08f1638a5aa06ec1eec3596716091dbc67d41159
louis.hameau <louis.hameau@campus-centre.fr>
2025-02-17 16:42:55 +0100

    Commit maj RS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faTwitter, faLinkedin, faSnapchatGhost } from '@fortawesome/free-brands-svg-icons';  // Import des icônes
+import { faInstagram, faLinkedin, faSnapchatGhost } from '@fortawesome/free-brands-svg-icons';  // Import des icônes
 
 function Footer(props) {
     return (
